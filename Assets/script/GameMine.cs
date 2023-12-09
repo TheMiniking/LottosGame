3a6cdf0aa5a1031ec2f4571fd69428f13dd7e88f
Miniking <110266113+TheMiniking@users.noreply.github.com>
2023-12-09 15:14:24 -0300

    02

---
+++ Assets/script/GameMine.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMine : MonoBehaviour
+{
+    [SerializeField] Wallet wallet;
+    [SerializeField] Slider mine;
+    [SerializeField] float timePlays;
+    [SerializeField] float downFinal;
+    [SerializeField] float downStop;
+    [SerializeField] float multResult;
+    [SerializeField] float speed = 0.001f;
+    [SerializeField] bool winBet = false;
+
+    [SerializeField] Bet atualBet;
+    [SerializeField] bool doABet;
+    [SerializeField] float valueToBet = 0.001f;
+    [SerializeField] string coin;
+    [SerializeField] MineState mineState = new MineState();
+    [SerializeField] Button bet;
+    [SerializeField] TMP_Text betButtonTxt;
+    [SerializeField] float stopValue;
+    [SerializeField] TMP_Text valueCoin, addressWallet;
+    [SerializeField] List<Image> gemPedregulho;
+    [SerializeField] List<Sprite> pedras;
+
+    private void Start()
+    {
+        if (wallet.player.address == "") wallet.StartWalletplusCoin();
+        addressWallet.text = wallet.player.address;
+        valueToBet = 0.001f;
+        coin = "BNB";
+        bet.onClick.AddListener(StopAndBet);
+        StartCoroutine(MineMachine());
+
+    }
+
+    private void FixedUpdate()
+    {
+        valueCoin.text = $"{wallet.player.coinBalance.ToString("0.00000000")} {wallet.player.atualCoin}";
+        
+        if (mineState.active)
+        {
+            downStop = downStop + speed * Time.deltaTime;
+            //multResult = CalcularValorProporcional(downStop); // Calcula o valor proporcional a partir do slider
+            speed = speed <= 0.01 ? speed+ 0.0005f : speed;
+            mine.value = downStop;
+            betButtonTxt.text = stopValue ==0 ? $"Stop on : x {downStop*100:0.00}": $"Get you bet X {stopValue * 100:0.00}%";
+            if(downStop >= downFinal)
+            {
+                CheckBetWin();
+            }
+        }else
+        {
+            betButtonTxt.text = doABet ? $"Wait Start" : $"Bet on Next :{valueToBet} {coin}";
+        }
+    }
+
+   
+    public IEnumerator MineMachine()
+    {
+        PrepareMine();
+        yield return new WaitForSeconds(timePlays);
+        bet.interactable = doABet;
+        StarMine();
+    }
+
+    public void MakeABet()
+    {
+        if (wallet.CheckBalance(valueToBet , coin))
+        {
+            atualBet = new Bet() { coin = coin, value = valueToBet, addressID = wallet.player.address };
+            wallet.PayBet(atualBet);
+            doABet = true;
+        }
+    }
+
+    public void StopAndBet()
+    {
+        if (!mineState.active)
+        {
+            MakeABet();
+        }
+        else
+        {
+            stopValue = downStop;
+            winBet = stopValue <= downFinal; 
+        }
+
+    }
+
+    public void PrepareMine()
+    {
+        mineState.active = false;
+        mineState.nextStop = Random.Range(0.01f, 0.999f);
+        downFinal = mineState.nextStop;
+        downStop = 0;
+        speed = 0.001f;
+        stopValue = 0;
+        winBet = false;
+        mine.value = 0;
+        bet.interactable = true;
+        doABet = false;
+        gemPedregulho.ForEach(x => { 
+            x.sprite = pedras[Random.Range(0, pedras.Count)];
+            x.gameObject.SetActive(Random.Range(0,2)==0? true : false);});
+
+    }
+
+    public void StarMine()
+    {
+        mineState.active = true;
+    }
+
+    public void CheckBetWin()
+    {
+        if (winBet)
+        {
+            wallet.CalculateBetAddToBalance(atualBet,downStop);
+        }
+        StartCoroutine(MineMachine());
+        
+    }
+}
+
+public class MineState
+{
+    public bool active;
+    public float nextStop = 0;
+}
---
+++ Assets/script/Wallet.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Wallet", menuName = "Zyan Asset/Wallet", order = 1)]
+public class Wallet : ScriptableObject
+{
+    public PlayerWallet player;
+
+    public void StartWalletplusCoin()
+    {
+        player = new PlayerWallet()
+        {
+            address = $"0x{UnityEngine.Random.Range(0, 999999):X6}",
+            coins = new List<Coin>()
+            {
+                { new Coin() { name = "Bitcoin", symbol = "BTC", contract = "0x0000000000000000000000000000000000000000", balance = 0 } },
+                { new Coin() { name = "Ethereum", symbol = "ETH", contract = "0x0000000000000000000000000000000000000000", balance = 0 } },
+                { new Coin() { name = "Litecoin", symbol = "LTC", contract = "0x0000000000000000000000000000000000000000", balance = 0 } },
+                { new Coin() { name = "Binance Coin", symbol = "BNB", contract = "0x0000000000000000000000000000000000000000", balance = 10 } }
+            }
+        };
+        UpdateAtualCoin("BNB");
+    }
+
+    public void UpdateAtualCoin(string coin)
+    {
+        player.atualCoin = coin;
+        player.coinBalance = player.coins.Find(x => x.symbol == coin).balance;
+        Debug.Log("Update Coin to " + coin);
+    }
+    public void PayBet(Bet bet)
+    {
+        player.coins.Find(x => x.symbol == bet.coin).balance -= bet.value;
+        UpdateAtualCoin(bet.coin);
+        Debug.Log("Pay bet " + bet.value);
+    }
+
+    public void WinBet(Bet bet)
+    {
+        player.coins.Find(x => x.symbol == bet.coin).balance += bet.value;
+        UpdateAtualCoin(bet.coin);
+        Debug.Log("Win Bet " + bet.value);
+    }
+
+    public float CalculateBetAddToBalance(Bet bet , float multiplier)
+    {
+        player.coins.Find(x => x.symbol == bet.coin).balance += bet.value *(multiplier*100);
+        UpdateAtualCoin(bet.coin);
+        Debug.Log($"Add to Balance x{ multiplier * 100:0.00}% Reward : {bet.value * multiplier * 100:0.00}" );
+        return bet.value * multiplier;
+    }
+
+    public bool CheckBalance(float value ,string coin)
+    {
+        if (player.coins.Find(x => x.symbol == coin).balance >= value)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
+[Serializable]
+public class PlayerWallet
+{
+    public string address;
+    public string atualCoin;
+    public float coinBalance;
+    public List<Coin> coins;
+
+}
+
+[Serializable]
+public class Coin
+{
+    public string name;
+    public string symbol;
+    public string contract;
+    public float balance;
+}
+[Serializable]
+public class Bet
+{
+    public string addressID;
+    public string coin;
+    public float value;
+}
