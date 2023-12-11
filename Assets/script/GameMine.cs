c671b5c7e3796ecd7321529f8cfdb680348bc180
Miniking <110266113+TheMiniking@users.noreply.github.com>
2023-12-11 19:24:25 -0300

    Atualizaçao grafica e atualizaçao de scripts

--- Assets/script/GameMine.cs
+++ Assets/script/GameMine.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class GameMine : MonoBehaviour
@@ -23,20 +24,47 @@ public class GameMine : MonoBehaviour
     [SerializeField] MineState mineState = new MineState();
     [SerializeField] Button bet;
     [SerializeField] TMP_Text betButtonTxt;
-    [SerializeField] float stopValue;
-    [SerializeField] TMP_Text valueCoin, addressWallet;
+    [SerializeField] float stopValue , stopValBet;
+    [SerializeField] TMP_Text valueCoin, addressWallet, betTxt, stopValBetTxt;
     [SerializeField] List<Image> gemPedregulho;
     [SerializeField] List<Sprite> pedras;
     [SerializeField] Material fundo;
 
+    [SerializeField] List<float> lastBets;
+    [SerializeField] List<GameObject> lastBetTxt;
+    [SerializeField] List<Button> stopBetListButton, betListButtonUpDown;
+    [SerializeField] Animator coruja;
+    [SerializeField] GameObject pedrasPoeira;
+
     private void Start()
     {
         if (wallet.player.address == "") wallet.StartWalletplusCoin();
-        addressWallet.text = wallet.player.address;
+        //addressWallet.text = wallet.player.address;
+        stopBetListButton.ForEach(x => x.onClick.RemoveAllListeners());
+        betListButtonUpDown.ForEach(x => x.onClick.RemoveAllListeners());
+        betListButtonUpDown[0].onClick.AddListener(() => UpDownBetAmount(0.001f, true,1));
+        betListButtonUpDown[1].onClick.AddListener(() => UpDownBetAmount(0.001f, false,1));
+        betListButtonUpDown[2].onClick.AddListener(() => UpDownBetAmount(0f, true, 2));
+        betListButtonUpDown[3].onClick.AddListener(() => UpDownBetAmount(0f, false,2));
+        betListButtonUpDown[4].onClick.AddListener(() => UpDownBetAmount(0f, true, 3));
+        betListButtonUpDown[5].onClick.AddListener(() => UpDownBetAmount(0f, false, 3));
+        stopBetListButton[0].onClick.AddListener(()=> UpDownAutoStop(0.1f,true));
+        stopBetListButton[1].onClick.AddListener(() => UpDownAutoStop(0.1f, false));
+        stopBetListButton[2].onClick.AddListener(() => UpDownAutoStop(1f, true));
+        stopBetListButton[3].onClick.AddListener(() => UpDownAutoStop(1f, false));
         valueToBet = 0.001f;
         coin = "BNB";
         bet.onClick.AddListener(StopAndBet);
-        fundo.SetFloat("_Velocity", 0);
+        pedrasPoeira.SetActive(false);
+        lastBetTxt.ForEach(x => {
+            if (lastBetTxt.IndexOf(x) <= lastBets.Count - 1)
+            {
+                x.gameObject.SetActive(true);
+                x.GetComponentInChildren<TMP_Text>().text = $"x{lastBets[lastBetTxt.IndexOf(x)]}";
+            }
+            else
+                x.gameObject.SetActive(false);
+        });
         StartCoroutine(MineMachine());
 
     }
@@ -45,13 +73,16 @@ public class GameMine : MonoBehaviour
     {
         valueCoin.text = $"{wallet.player.coinBalance.ToString("0.00000000")} {wallet.player.atualCoin}";
         fundo.SetFloat("_Velocity", speed != 0.001f ? 0.5f : 0);
+        betTxt.text = $"{valueToBet:0.0000} {coin}";
+        if(terminouOTempo)uiTimer.text = $"x{downStop * 100:0.00}";
+        stopValBetTxt.text = $"x {stopValBet:0.00}";
         if (mineState.active)
         {
             downStop = downStop + speed * Time.deltaTime;
             //multResult = CalcularValorProporcional(downStop); // Calcula o valor proporcional a partir do slider
             speed = speed <= 0.01 ? speed+ 0.0005f : speed;
             mine.value = downStop;
-            betButtonTxt.text = stopValue ==0 ? $"Stop on : x {downStop*100:0.00}": $"Get you bet X {stopValue * 100:0.00}%";
+            betButtonTxt.text = stopValue ==0 ? $"Stop Now : x {downStop*100:0.00}": $"Get you bet X {stopValue * 100:0.00}%";
             if(downStop >= downFinal)
             {
                 CheckBetWin();
@@ -62,7 +93,20 @@ public class GameMine : MonoBehaviour
         }
     }
 
-   
+    public void UpDownAutoStop(float toUpVal, bool toUp)
+    {
+        stopValBet = toUp? stopValBet + toUpVal : stopValBet - toUpVal;
+        stopValBet = stopValBet < 1.01 ? 1.01f : stopValBet;
+    }
+
+    public void UpDownBetAmount(float toUpVal, bool toUp, int sp)
+    {
+
+        if (toUp) valueToBet = sp == 1 ? valueToBet + toUpVal : sp== 2? valueToBet*2 : wallet.player.coinBalance;
+        else valueToBet = sp == 1 ? valueToBet - toUpVal : sp == 2 ? valueToBet / 2 : 0.001f;
+        valueToBet = valueToBet < 0.001 ? 0.001f : valueToBet > wallet.player.coinBalance ? wallet.player.coinBalance :  valueToBet;
+    }
+
     public IEnumerator MineMachine()
     {
         PrepareMine();
@@ -71,6 +115,7 @@ public class GameMine : MonoBehaviour
         StarMine();
     }
 
+    private void OnDisable() => fundo.SetFloat("_Velocity", 0);
     public void MakeABet()
     {
         if (wallet.CheckBalance(valueToBet , coin))
@@ -111,22 +156,68 @@ public class GameMine : MonoBehaviour
         gemPedregulho.ForEach(x => { 
             x.sprite = pedras[Random.Range(0, pedras.Count)];
             x.gameObject.SetActive(Random.Range(0,2)==0? true : false);});
-
+        Inicio((int)timePlays);
     }
 
     public void StarMine()
     {
         mineState.active = true;
+        coruja.Play("Down");
+        pedrasPoeira.SetActive(true);
     }
 
     public void CheckBetWin()
     {
+        coruja.Play("Kabum");
+        pedrasPoeira.SetActive(false);
         if (winBet)
         {
             wallet.CalculateBetAddToBalance(atualBet,downStop);
         }
+        if (lastBets.Count >= 5) lastBets.RemoveAt(0);
+        lastBets.Add(float.Parse($"{downFinal*100:0.00}"));
+        lastBetTxt.ForEach(x => {
+            if (lastBetTxt.IndexOf(x) <= lastBets.Count - 1)
+            {
+                x.gameObject.SetActive(true);
+                x.GetComponentInChildren<TMP_Text>().text = $"x{lastBets[lastBetTxt.IndexOf(x)]}";
+                x.GetComponent<Image>().color = lastBets[lastBetTxt.IndexOf(x)] <= 2 ? new Color(1f,0f,0f,0.15f) : lastBets[lastBetTxt.IndexOf(x)] <= 10 ? new Color(0f, 1f, 0f, 0.15f) : new Color(1f, 0.5f, 0f, 0.15f);
+            }
+            else
+                x.gameObject.SetActive(false);
+        });
         StartCoroutine(MineMachine());
         
+    }
+
+    [SerializeField] Image uiFill;
+    [SerializeField] TMP_Text uiTimer;
+    public int duration;
+    int remaningDuration;
+    public bool terminouOTempo;
+
+    public void Inicio(int second)
+    {
+        terminouOTempo = false;
+        remaningDuration = second;
+        StartCoroutine(UpdateTime());
+    }
+
+    private IEnumerator UpdateTime()
+    {
+        while (remaningDuration >= 0)
+        {
+            uiTimer.text = $"{remaningDuration / 60:00} : {remaningDuration % 60:00}";
+            uiFill.fillAmount = Mathf.InverseLerp(0, duration, remaningDuration);
+            remaningDuration--;
+            yield return new WaitForSeconds(1f);
+        }
+        EndOfTime();
+    }
+
+    public void EndOfTime()
+    {
+        terminouOTempo = true;
     }
 }
 
