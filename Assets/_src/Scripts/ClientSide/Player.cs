d7dc325ca04f1e001c36ee5479b286effc13aab5
Miniking <110266113+TheMiniking@users.noreply.github.com>
2024-05-20 18:05:34 -0300

    20/05

--- Assets/_src/Scripts/ClientSide/Player.cs
+++ Assets/_src/Scripts/ClientSide/Player.cs
@@ -5,22 +5,30 @@ public class Player : MonoBehaviour
     [SerializeField] Animator anim;
     [SerializeField] GameObject explosion;
     [SerializeField] GameObject fire;
+    [SerializeField] int tankNum;
+    [SerializeField] bool lastMovingStatus = false;
 
     void Start()
     {
+
+    }
+
+    private void Update()
+    {
+        if(GameManager.Instance.fundoOnMove != lastMovingStatus)
+        {
+            lastMovingStatus = GameManager.Instance.fundoOnMove;
+            Walking(lastMovingStatus);
+        }
     }
 
     public void Walking(bool v)
     {
-        // Debug.Log("Animation:"+( v? "Walking": "Lost"));
         anim.Play(v ? "Walking" : "Lost");
-        //anim.SetBool("walking", v);
     }
     public void Crash(bool v)
     {
         Debug.Log("Crash");
         anim.Play("Lost");
-        //explosion.SetActive(v);
-        //fire.SetActive(v);
     }
 }
